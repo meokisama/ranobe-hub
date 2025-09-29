160bf4e9ec6514824a3c7674634c94c2bec49377
meokisama <hoangdinhsang123@gmail.com>
2025-09-29 21:02:27 +0700

    feat: redirect to root instead 404

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,12 +1,18 @@
-const Custom404 = () => {
-  return (
-    <div className="flex justify-center items-center h-screen w-full px-4 text-xl lg:text-2xl font-bold bg-[#111827] text-white fixed top-0 z-30">
-      <p>
-        Oops! The page you seek has gone on a quest for adventure and may never
-        return. 💀
-      </p>
-    </div>
-  );
-};
+import { redirect } from "next/navigation";
 
-export default Custom404;
+export default function NotFound() {
+  redirect("/");
+}
+
+// const Custom404 = () => {
+//   return (
+//     <div className="flex justify-center items-center h-screen w-full px-4 text-xl lg:text-2xl font-bold bg-[#111827] text-white fixed top-0 z-30">
+//       <p>
+//         Oops! The page you seek has gone on a quest for adventure and may never
+//         return. 💀
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Custom404;
